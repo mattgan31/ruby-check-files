8606f03feeb41bbe910c8b68c6ab0186416e5bf2
Rahmat Alamsyah <57090921+mattgan31@users.noreply.github.com>
2023-01-24 16:18:49 +0700

    Add error handling if path is empty or wrong

--- app.rb
+++ app.rb
@@ -4,6 +4,11 @@ files = Array.new
 i = 0
 max = 0
 
+if path == ""
+    puts "\nPlease enter the path!"
+    return
+end
+
 Dir.glob("#{path}/*") do |file_path|
     file = File.basename(file_path, ".*")
     files.append(file)
@@ -15,11 +20,15 @@ files.each do |value|
     b[value]+= 1
 end
 
-puts "\nResult: "
-
 max_key = b.key(b.values.max)
 max_value = b.values.max
 
+if max_value == nil
+    puts "\nPlease enter the correct path!"
+    return
+end
+
+puts "\nResult: "
 unless max_value >= 2
     puts "Sorry, there are no duplicate filenames"
 else
